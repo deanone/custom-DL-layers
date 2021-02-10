9e5839685b99a2bb0436f173abd64e1977da0be2
deanone <athanasios.salamanis@gmail.com>
2021-02-10 18:02:12 +0200

    Initial implementation of GCNLayer.

--- gcn/custom_gcn.py
+++ gcn/custom_gcn.py
@@ -1,2 +1,23 @@
 import tensorflow as tf
 from tensorflow import keras
+
+
+class GCNLayer(keras.layers.Layer):
+    def __init__(self, F=32, A_norm):
+        super(GCNLayer, self).__init__()
+        self.F = F
+        self.A_norm = A_norm
+        self.N = A_norm.shape[0]
+
+    def build(self, input_shape):
+        self.theta = self.add_weight(
+            shape=(input_shape[-1], self.F),
+            initializer="random_normal",
+            trainable=True,
+        )
+        self.b = self.add_weight(
+            shape=(N,), initializer="random_normal", trainable=True
+        )
+
+    def call(self, X):
+        return tf.nn.relu(tf.matmul(tf.matmul(self.A_norm, X), self.theta) + self.b)
