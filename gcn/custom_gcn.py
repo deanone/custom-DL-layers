e18d0c6ea3d5950fffd474863f6d214389c9df5c
deanone <athanasios.salamanis@gmail.com>
2021-02-11 14:34:31 +0200

    Completed the implementation of the GCN layer and model (of Kipf and Welling). Initial attempt for running the training procedure resulted in the following bug -- TypeError: Cannot interpret 'tf.float32' as a data type --, which must be resolved in order to proceed.

--- gcn/custom_gcn.py
+++ gcn/custom_gcn.py
@@ -9,7 +9,7 @@ class GCNLayer(keras.layers.Layer):
     This class subclasses the keras.layers.Layer base class.
 
     """
-    def __init__(self, C = 32, A_norm):
+    def __init__(self, C, A_norm, activation_type):
         """
         Constructor.
         :param C: the number of channels/filter of the layer
@@ -22,6 +22,7 @@ class GCNLayer(keras.layers.Layer):
         self.C = C
         self.A_norm = A_norm
         self.N = A_norm.shape[0]    #   number of nodes in the graph
+        self.activation_type = activation_type
 
     def build(self, input_shape):
         self.theta = self.add_weight(
@@ -29,7 +30,7 @@ class GCNLayer(keras.layers.Layer):
             initializer = "random_normal",
             trainable = True,
         )
-        self.b = self.add_weight(shape = (N,), initializer = "random_normal", trainable = True)
+        self.b = self.add_weight(shape = (self.N,), initializer = "random_normal", trainable = True)
 
     def call(self, X):
         """
@@ -38,7 +39,15 @@ class GCNLayer(keras.layers.Layer):
         :type X: numpy.ndarray
 
         """
-        return tf.nn.relu(tf.matmul(tf.matmul(self.A_norm, X), self.theta) + self.b)
+
+        self.A_norm = self.A_norm.astype(X.dtype)
+        self.theta = self.theta.astype(X.dtype)
+        self.b = self.b.astype(X.dtype)
+
+        if self.activation_type == 'relu':
+            return tf.nn.relu(tf.matmul(tf.matmul(self.A_norm, X), self.theta) + self.b)
+        elif self.activation_type == 'softmax':
+            return tf.nn.softmax(tf.matmul(tf.matmul(self.A_norm, X), self.theta) + self.b)
 
 
 class GCN(keras.Model):
@@ -53,18 +62,15 @@ class GCN(keras.Model):
         self,
         num_units_in_hidden_layers,
         num_units_in_output_layer,
+        A_norm,
         name='gcn',
         **kwargs
     ):
         super(GCN, self).__init__(name=name, **kwargs)
-        self.layer_1 = TypicalNNHiddenLayer(num_units_in_hidden_layers[0])
-        self.layer_2 = TypicalNNHiddenLayer(num_units_in_hidden_layers[1])
-        self.layer_3 = TypicalNNHiddenLayer(num_units_in_hidden_layers[2])
-        self.layer_out = keras.layers.Dense(num_units_in_output_layer)
+        self.layer_1 = GCNLayer(num_units_in_hidden_layers[0], A_norm, 'relu')
+        self.layer_out = GCNLayer(num_units_in_output_layer, A_norm, 'softmax')
 
-    def call(self, inputs):
-        x = self.layer_1(inputs)
-        x = self.layer_2(x)
-        x = self.layer_3(x)
-        x = self.layer_out(x)
-        return x
+    def call(self, X):
+        H = self.layer_1(X)
+        Z = self.layer_out(H)
+        return Z
--- gcn/main.py
+++ gcn/main.py
@@ -1,15 +1,39 @@
+import tensorflow as tf
 from preprocessing import *
+from custom_gcn import GCN
+
 
 def main():
+
+	print('Creating graph...')
 	graph_filename = 'data/citeseer/citeseer.cites'
-	features_filename = 'data/citeseer/citeseer.content'
 	g = create_graph_citeseer(graph_filename)
+
+	print('Creating normalized adjacency matrix...')
 	A_norm = create_A_norm(g)
+
+	print('Loading nodes feature matrix...')
+	features_filename = 'data/citeseer/citeseer.content'
 	F, y = create_feature_matrix_citeseer(features_filename, list(g.nodes))
-	res = A_norm.dot(F)
-	print('A normalized: ', A_norm.shape)
-	print('Feature matrix: ', F.shape)
-	print('Their product: ', res.shape)
+
+	# Split data
+	print('Splitting data into training and test subsets...')
+	F, y_train_masked, y_test_masked = create_train_test_data(F, y, 1000)
+
+	# Set up, train and evaluate the custom DNN model
+	print('Setting up GCN...')
+	num_units_in_hidden_layers = [32]
+	num_units_in_output_layer = 6
+	gcn = GCN(num_units_in_hidden_layers, num_units_in_output_layer, A_norm)
+	gcn.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-2), loss='categorical_crossentropy', metrics=['accuracy'])
+
+	print('Training GCN...')
+	gcn.fit(F, y_train_masked, epochs=5, batch_size=100)
+	
+
+	#evaluation_results_custom = dnn_custom.evaluate(x_test, y_test)
+	#print('Test MAE (custom DNN): ', evaluation_results_custom[1])
+
 
 
 if __name__ == '__main__':
